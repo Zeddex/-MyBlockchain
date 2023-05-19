731cc6a140665fa021fa51a2d798b6cca9a90b76
Zeddex <zedxlab@gmail.com>
2023-05-19 19:49:13 +0200

    initial commit

---
+++ Block.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlockchain
+{
+    internal class Block
+    {
+        public string Timestamp { get; set; }
+        public string Data { get; set; }
+        public string Hash { get; set; }
+        public string PrevHash { get; set; } = string.Empty;
+
+        public Block(string data)
+        {
+            Data = data;
+            Timestamp = Calc.GetTimestamp();
+            Hash = Calc.GetHash(PrevHash, Timestamp, Data);
+        }
+    }
+}
---
+++ Calc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace MyBlockchain
+{
+    internal class Calc
+    {
+        public static string GetHash(string prevHash, string timestamp, string data)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(prevHash + timestamp + data);
+            var inputHash = SHA256.HashData(inputBytes);
+            return Convert.ToHexString(inputHash);
+        }
+
+        public static string GetTimestamp()
+        {
+            var timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+
+            return timestamp.ToString();
+        }
+    }
+}
